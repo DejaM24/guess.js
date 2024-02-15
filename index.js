dd5d415929fbf0a5d6cd7398086d873e94660048
Deja M <deja.moore2018@gmail.com-- listls -acd>
2024-02-15 00:01:11 -0500

    second submit

--- index.js
+++ index.js
@@ -14,43 +14,43 @@ async function start() {
   console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.")
   let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
   console.log('You entered: ' + secretNumber);
-//computer generates guess to secretNumber
-let min = 1
-let max = 100
-let guess = Math.round((min + max) / 2);
- 
+  //computer generates guess to secretNumber
+  let min = 1
+  let max = 100
+  let guess = Math.round((min + max) / 2);
+
   //the computer starting to guess secretNumber
-let input = await ask(`Is your number...${guess} y or n`)
-console.log(input);
+  let input = await ask(`Is your number...${guess} (y) or (n)`)
+  console.log(input);
 
+  //the computer startes checking if secretNumber is higher or lower than guess
   if (input = "n") {
-    while (guess !== secretNumber) {
-      input = await ask(`Is your number...${guess} y or n`)
-      }
-}
+  } while (guess !== secretNumber) {
+    let question = await ask(`Is your number (h)igher or (l)ower?`)
+    console.log(question);
+  } 
+  
+  //if secretNumber is higher than guess ...
+    if (question = "h") {
+      console.log()
+    } 
+
+  // if secretNumber is lower than guess ...
+  if (input = "l") {
+    console.log();
+  }
+
+  // y = secretNumber
+  // console.log("Yay! I got it! Good game.")
+  // // n = secretNumber
+
+  if (input === "y") {
+    console.log("Yay! I guessed it! Good game.");
+  }
 
 
 
 
-
-
-
-
-
-
-
-
-// y = secretNumber
-// console.log("Yay! I got it! Good game.")
-// // n = secretNumber
-
-// if (guess === secretNumber) {
-//   console.log("You guessed it!");
-// }
-
-
-
- 
 
   // while (secretNumber !== guess)
   //   if (guess < secretNumber) {
@@ -59,6 +59,6 @@ console.log(input);
   //     guess = await ask("Sorry to high. Guess lower!")
   //   }
 
-  
+
   process.exit()
 }
