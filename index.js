a24d9aec0b32b8c53f55ccc074953cb1c46f7561
Deja M <deja.moore2018@gmail.com-- listls -acd>
2024-02-14 23:20:14 -0500

    first submit

--- index.js
+++ index.js
@@ -13,23 +13,52 @@ start();
 async function start() {
   console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.")
   let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
-    console.log('You entered: ' + secretNumber);
+  console.log('You entered: ' + secretNumber);
+//computer generates guess to secretNumber
+let min = 1
+let max = 100
+let guess = Math.round((min + max) / 2);
+ 
+  //the computer starting to guess secretNumber
+let input = await ask(`Is your number...${guess} y or n`)
+console.log(input);
 
-  let yourSecretNumber = await input(number);
-  console.log(yourSecretNumber);
+  if (input = "n") {
+    while (guess !== secretNumber) {
+      input = await ask(`Is your number...${guess} y or n`)
+      }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// y = secretNumber
+// console.log("Yay! I got it! Good game.")
+// // n = secretNumber
+
+// if (guess === secretNumber) {
+//   console.log("You guessed it!");
+// }
+
+
+
+ 
+
+  // while (secretNumber !== guess)
+  //   if (guess < secretNumber) {
+  //     guess = await ask("Sorry too low. Guess higher!")
+  //   } else {
+  //     guess = await ask("Sorry to high. Guess lower!")
+  //   }
+
   
-
-
-  if(secretNumber = guess) {
-    console.log("You guessed it!");
-  }
-
-while(secretNumber !== guess)
-	if(guess < secretNumber) {
-        guess = await ask("Sorry too low. Guess higher!")
-	} else(guess > secretNumber) 
-	guess = await ask("Sorry to high. Guess lower!")
-    }
-  
-
-  process.exit();
+  process.exit()
+}
