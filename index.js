3a20ec4db69bd0698efe6f25dbd09e024dda6309
Deja M <deja.moore2018@gmail.com-- listls -acd>
2024-02-17 20:11:59 -0500

    second submit

--- index.js
+++ index.js
@@ -14,51 +14,43 @@ async function start() {
   console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.")
   let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
   console.log('You entered: ' + secretNumber);
-  //computer generates guess to secretNumber
-  let min = 1
-  let max = 100
-  let guess = Math.round((min + max) / 2);
 
-  //the computer starting to guess secretNumber
-  let input = await ask(`Is your number...${guess} (y) or (n)`)
-  console.log(input);
+  //computer generates a random number and sets it as a guess 
 
-  //the computer startes checking if secretNumber is higher or lower than guess
-  if (input = "n") {
-  } while (guess !== secretNumber) {
-    let question = await ask(`Is your number (h)igher or (l)ower?`)
-    console.log(question);
-  } 
-  
-  //if secretNumber is higher than guess ...
-    if (question = "h") {
-      console.log()
-    } 
-
-  // if secretNumber is lower than guess ...
-  if (input = "l") {
-    console.log();
+  function randomNum(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
-  // y = secretNumber
-  // console.log("Yay! I got it! Good game.")
-  // // n = secretNumber
+  let guess = randomNum(1, 100);
 
-  if (input === "y") {
-    console.log("Yay! I guessed it! Good game.");
+  //the guessing begins 
+  while (guess !== secretNumber) {
+    function randomNum(min, max) {
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    let guess = randomNum(1, 100);
+
+    //the computer starts to guess the secretNumber
+    let input = await ask(`Is your number...${guess}? (y) or (n) > `)
+    console.log(input);
+
+    //if guess is correct, computer exits, if guess is incorrect, it continues guessing
+    if (input === "y") {
+      console.log("Yay! I guessed it! Good game.")
+      break;
+    } else {
+
+      //the computer starts checking if the secretNumber is higher or lower than guess
+      let question = await ask(`Is your number (h)igher or (l)ower than ${guess}? > `)
+      console.log(question);
+      if (question === "l") {
+        let tempMax = guess - 1;
+      } else {
+        let tempMin = guess + 1;
+      }
+
+    }
   }
-
-
-
-
-
-  // while (secretNumber !== guess)
-  //   if (guess < secretNumber) {
-  //     guess = await ask("Sorry too low. Guess higher!")
-  //   } else {
-  //     guess = await ask("Sorry to high. Guess lower!")
-  //   }
-
-
   process.exit()
 }
