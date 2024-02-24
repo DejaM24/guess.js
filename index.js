c53fd859983d4154b5632d242c5a9e7000da76a2
Deja M <deja.moore2018@gmail.com-- listls -acd>
2024-02-23 23:35:28 -0500

    second submit

--- index.js
+++ index.js
@@ -23,34 +23,29 @@ async function start() {
 
   let guess = randomNum(1, 100);
 
+  min = 1;
+  max = 100;
+
   //the guessing begins 
   while (guess !== secretNumber) {
-    function randomNum(min, max) {
-      return Math.floor(Math.random() * (max - min + 1) + min);
-    }
-
-    let guess = randomNum(1, 100);
+    let tempMin = min;
+    let tempMax = max;
+    let guess = randomNum(tempMin, tempMax);
 
     //the computer starts to guess the secretNumber
     let input = await ask(`Is your number...${guess}? (y) or (n) > `)
-    console.log(input);
-
-    //if guess is correct, computer exits, if guess is incorrect, it continues guessing
-    if (input === "y") {
+    if (input === "n") {
+      let question = await ask(`Is your number (h)igher or (l)ower than ${guess}? > `)
+      if (question === "l") {
+        tempMax = guess - 1;
+      } else if (question === "h") {
+        tempMin = guess + 1;
+      } 
+    } else {
       console.log("Yay! I guessed it! Good game.")
       break;
-    } else {
-
-      //the computer starts checking if the secretNumber is higher or lower than guess
-      let question = await ask(`Is your number (h)igher or (l)ower than ${guess}? > `)
-      console.log(question);
-      if (question === "l") {
-        let tempMax = guess - 1;
-      } else {
-        let tempMin = guess + 1;
-      }
-
     }
+
   }
   process.exit()
 }
