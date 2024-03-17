115e57750e3f9081218dd776b9f6594578696453
Deja M <deja.moore2018@gmail.com-- listls -acd>
2024-03-17 18:37:34 -0400

    third submit

--- index.js
+++ index.js
@@ -21,31 +21,69 @@ async function start() {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
-  let guess = randomNum(1, 100);
+  let guessing = true;
 
   min = 1;
   max = 100;
 
   //the guessing begins 
-  while (guess !== secretNumber) {
+  while (guessing) {
     let tempMin = min;
     let tempMax = max;
+
+    //generates a guess
     let guess = randomNum(tempMin, tempMax);
 
-    //the computer starts to guess the secretNumber
-    let input = await ask(`Is your number...${guess}? (y) or (n) > `)
-    if (input === "n") {
-      let question = await ask(`Is your number (h)igher or (l)ower than ${guess}? > `)
+    //fact checks that min isn't greater than max
+    do {
+      if (tempMin > tempMax) {
+        console.log("Sorry, that doesn't seem to be possible.")
+      }
+
+      tempMin = min;
+      tempMax = max;
+
+      //checks if input is valid and starts guessing 
+      let input = null;
+      do {
+        if (input !== null) {
+          console.log("Sorry bad input. Please type y or n.");
+        }
+        input = await ask(`Is your number...${guess}? (y) or (n) > `);
+      } while (input !== "" && input !== "n" && input !== "y");
+
+      //computer checks if input is correct & breaks out of game if it is
+      if (input === "y") {
+        guessing = false;
+        break;
+        //fact checks input if input was correct
+      } else {
+        if (tempMin === tempMax) {
+          console.log(`That can't be right. ${guess} has to be your number. Nice try.`)
+          guessing = false;
+          break;
+        }
+      }
+      //checks if input is valid
+      let question = null;
+      do {
+        if (question !== null) {
+          console.log("Sorry bad input. Please type h or l.")
+        }
+        question = await ask(`Is your number (h)igher or (l)ower than ${guess}? > `)
+      } while (question !== "l" && question !== "h");
+      //checks if input was lower or higher
       if (question === "l") {
         tempMax = guess - 1;
       } else if (question === "h") {
         tempMin = guess + 1;
-      } 
-    } else {
-      console.log("Yay! I guessed it! Good game.")
-      break;
+      }
     }
+    while (tempMin > tempMax);
 
+    min = tempMin;
+    max = tempMax;
   }
+  console.log("Great! I guessed your number! Good game.")
   process.exit()
 }
